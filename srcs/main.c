5cb1a736f50100101334e490de8fec93cf16ee92
Elias ZANOTTI <zanotti.elias@gmail.com>
2022-12-27 19:06:55 +0100

    change file : add execute fonction in main nrm

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,30 @@
 /*   By: elias <zanotti.elias@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 14:15:59 by elias             #+#    #+#             */
-/*   Updated: 2022/12/27 18:42:34 by elias            ###   ########.fr       */
+/*   Updated: 2022/12/27 19:06:36 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_execute_command(t_args *args)
+{
+	(void)args;
+	//TODO
+	return (0);
+}
+
 int	ft_prompt_loop(t_args *args)
 {
+	int	error_code;
+
 	while (!args->exit_code)
 	{
 		args->command = readline(args->prompt);
 		args->command_list = ft_split(args->command, ' ');
+		error_code = ft_execute_command(args);
+		if (error_code)
+			return (error_code);
 		if (args->command_list[0] && \
 				!ft_strcmp(args->command_list[0], "exit"))
 			args->exit_code = 1;
@@ -33,12 +45,12 @@ int	main(int argc, char **argv)
 	args = malloc(sizeof(t_args));
 	if (!args)
 		return (ft_error(99));
-	error_code = ft_struct_init(args); 
+	error_code = ft_struct_init(args);
 	if (error_code)
 		return (ft_error(error_code));
-
-	ft_prompt_loop(args);
-
+	error_code = ft_prompt_loop(args);
+	if (error_code)
+		return (ft_error(error_code));
 	(void)argc;
 	(void)argv;
 	return (0);
