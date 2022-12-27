ffdfefb54e7b7a6d8d0b6775531c944c8c88a2dc
Elias ZANOTTI <zanotti.elias@gmail.com>
2022-12-27 17:21:08 +0100

    change README

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 ## Error code
 
-- 99 : Malloc error
+- <span style="color:red">[ERROR:99]</span> Malloc cannot be created
 
 ## Bonnes pratiques
 
 Toujours nommer les fonctions avec comme prefix ft (ex: ft\_function, ft\_print)
 
 Bien penser a creer une branche pour chaque features creee 
+
+Trier les protypes de fonctions par ordre alphabetique
+
+
---
+++ srcs/ft_error.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elias <zanotti.elias@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 17:14:22 by elias             #+#    #+#             */
+/*   Updated: 2022/12/27 17:15:46 by elias            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_error(int error_code)
+{
+	if (error_code)
+		printf("\e[1;31m[ERROR:%d]\e[0m ", error_code);
+	if (error_code == 99)
+		printf("Malloc cannot be created\n");
+	return (error_code);
+}
