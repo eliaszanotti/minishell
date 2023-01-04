6596c79f2e7ecb27e05fc75cacd7aa92907a0feb
Thibault Giraudon <tgiraudo@z2r8p8.42lyon.fr>
2023-01-04 14:10:34 +0100

    save delimiter

--- srcs/ft_utils.c
+++ srcs/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:43:07 by elias             #+#    #+#             */
-/*   Updated: 2023/01/04 13:19:47 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2023/01/04 14:09:09 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,14 @@ int	ft_is_delimiter(char *str)
 	return (0);
 }
 
-void	ft_get_delimiter(char *cmd, t_args *args)
+void	ft_count_delimiter(char *cmd, t_args *args)
 {
 	int i;
+	int j;
 	int count;
 
 	i = 0;
+	j = 0;
 	count = 0;
 	while (cmd[i])
 	{
@@ -35,11 +37,43 @@ void	ft_get_delimiter(char *cmd, t_args *args)
 		if (cmd[i] == '>' || cmd[i] == '<')
 		{
 			if (cmd[i + 1] == cmd[i])
+			{
 				i++;
+			}
 			count++;
 		}
 		i++;
 	}
-	printf("%d", count);
-	(void)args;
+	args->delimiter = malloc(sizeof(char *) * (count + 1));
+	args->delimiter[count + 1] = NULL;
+}
+
+void	ft_get_delimiter(char *cmd, t_args *args)
+{
+	int index;
+	int j;
+	
+	index = 0;
+	ft_count_delimiter(cmd, args);
+	while(*cmd)
+	{
+		j = 0;
+		if(*cmd == '|')
+			j++;
+		if(*cmd == '>' || *cmd == '<')
+		{
+			if (*(cmd + 1) == *cmd)
+				j++;
+			j++;
+		}
+		if (j != 0)
+		{
+			args->delimiter[index] = malloc(sizeof(char) * (j + 1));
+			args->delimiter[index++] = ft_substr(cmd, 0, j);
+			printf("[%s]\n", args->delimiter[index - 1]);
+		}
+		if (j == 2)
+			cmd++;
+		cmd++;
+	}
 }
