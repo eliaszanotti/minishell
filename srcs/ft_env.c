72a1db7f0e977b6d768b0e5288f163b21d1fad5f
Thibault Giraudon <tgiraudo@z2r8p8.42lyon.fr>
2023-01-04 18:56:53 +0100

    start remove leaks

--- srcs/ft_env.c
+++ srcs/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 17:07:25 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/01/04 17:38:45 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2023/01/04 18:56:21 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@ char	*replace_env(char *str)
 {
 	int		i;
 	int		j;
+	char	*tmp;
 	char	*name;
 	char	*new_str;
 
 	i = 0;
 	while (str[i] && str[i] != '$')
 		i++;
-	new_str = ft_substr(str, 1, i);
+	tmp = ft_substr(str, 0, i);
 	j = i;
 	while (str[j] && str[j] != ' ')
 		j++;
 	name = ft_substr(str, i + 1, j - i - 1);
 	name = getenv(name);
 	if (name)
-		new_str = ft_strjoin(new_str, name);
-	new_str = ft_strjoin(new_str, str + j);
-	new_str = ft_substr(new_str, 0, ft_strlen(new_str) - 1);
-	return (new_str);
+		new_str = ft_strjoin(tmp, name);
+	free(tmp);
+	tmp = ft_strjoin(new_str, str + j);
+	// new_str = ft_substr(new_str, 0, ft_strlen(new_str) - 1);
+	free(name);
+	return (tmp);
 }
