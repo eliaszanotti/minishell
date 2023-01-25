6ce3526eeed3e2e2f98f03b6d30872d558bc40d4
Thibault Giraudon <thibault.giraudon@gmail.com>
2023-01-25 18:02:00 +0100

    Export working

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 13:32:20 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/01/25 13:39:33 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2023/01/25 15:44:36 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,15 @@
 typedef struct s_args	t_args;
 
 //	ft_echo.c
-int	ft_echo(char **cmd);
+int		ft_echo(char **cmd);
 //	ft_exec_builtins.c
-int	ft_exec_builtins(t_args *args);
+int		ft_exec_builtins(t_args *args);
 //	ft_export.c
-int	ft_export(char **cmd, t_args *args);
+int		ft_export(char **cmd, t_args *args);
 //	ft_open_dir.c
-int	ft_open_dir(char **cmd);
+int		ft_open_dir(char **cmd);
 //	ft_unset.c
-int	ft_unset(char **cmd, t_args *args);
+int		ft_unset(char **cmd, t_args *args);
+char	*ft_get_var_name(char	*var);
 
 #endif
--- srcs/builtins/ft_exec_builtins.c
+++ srcs/builtins/ft_exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 11:31:16 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/01/24 16:05:25 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2023/01/25 16:09:29 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int	ft_exec_builtins(t_args *args)
 	{
 		if(ft_export(args->stack[0], args))//TODO error malloc
 			return (ft_error(99));
+		printf("\n\n\nOK\n\n\n");
 		return (1);	
 	}
 	else if (!ft_strcmp(args->stack[0][0], "unset"))
