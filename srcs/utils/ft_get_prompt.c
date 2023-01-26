a48cb7f451a63ffa21de1d4dba5ed4019afbc927
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-01-26 18:51:29 +0100

    change file : fix prompt

--- srcs/utils/ft_get_prompt.c
+++ srcs/utils/ft_get_prompt.c
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 15:35:20 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/01/26 14:43:35 by elias            ###   ########.fr       */
+/*   Updated: 2023/01/26 18:50:20 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ char	*ft_get_prompt(char *cwd) // TODO segfault with env -i
 		while (cwd[i] != '/')
 			i--;
 		if (!ft_strcmp(cwd, getenv("HOME")))
-			cwd = ft_strjoin(" ⚡\e[46;90m\ue0b0 ", "~");
+			cwd = ft_strjoin(" ⚡\001\e[46;90m\ue0b0\002 ", "~");
 		else
-			cwd = ft_strjoin(" ⚡\e[46;90m\ue0b0 ", cwd + i + 1);
+			cwd = ft_strjoin(" ⚡\001\e[46;90m\ue0b0\002 ", cwd + i + 1);
 		tmp = getenv("LOGNAME");
 		tmp2 = ft_strjoin("\001\e[100;36m ", tmp);
 		tmp = ft_strjoin(tmp2, cwd);
 		free(tmp2);
 		free(cwd);
-		cwd = ft_strjoin(tmp, " \001\e[36;49m\002\ue0b0\001\e[0m\002 ");
+		cwd = ft_strjoin(tmp, " \001\e[36;49m\ue0b0\e[0m\002 ");
 		free(tmp);
 		return (cwd);
 	}
