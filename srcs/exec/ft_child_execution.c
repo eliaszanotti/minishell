d6bb306c770513d0d2d38388636ba34fb99422e1
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-02-14 15:16:04 +0100

    merge

---
+++ srcs/exec/ft_child_execution.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_child_execution.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 18:44:24 by ezanotti          #+#    #+#             */
+/*   Updated: 2023/02/13 19:31:44 by ezanotti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*static int	ft_dup_and_exec(t_args *args, char **command, int last, int fd[2])
+{
+	char	**char_envp;
+	char	*path;
+
+	if (args->infile && dup2(args->infile, STDIN_FILENO) == -1)
+		return (ft_error(13));
+	else if (dup2(args->fdd, STDIN_FILENO) == -1)
+		return (ft_error(13));
+	if (!last && dup2(fd[1], STDOUT_FILENO) == -1)
+		return (ft_error(13));
+	if (args->outfile && dup2(args->outfile, STDOUT_FILENO) == -1)
+		return (ft_error(13));
+	close(fd[0]);
+	if (ft_is_char_builtins(command[0]) && !ft_exec_builtins(args, command))
+		exit(0);
+	path = ft_get_path(args, command[0]);
+	char_envp = ft_get_char_envp(args);
+	if (execve(path, command, char_envp) == -1)
+		return (ft_free_str(char_envp), free(path), ft_error(12));
+	ft_free_str(char_envp);
+	free(path);
+	return (0);
+}
+
+int	ft_execute_child(t_args *args, char **command, int last)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	if (pipe(fd))
+		return (ft_error(11));
+	if (last && args->size == 1 && ft_is_char_builtins(command[0]))
+		return (ft_exec_builtins(args, command));
+	pid = fork();
+	if (pid == -1)
+		return (ft_error(4));
+	if (pid == 0 && ft_dup_and_exec(args, command, last, fd))
+		return (1);
+	close(fd[1]);
+	ft_add_pid(args, pid);
+	args->infile = STDIN_FILENO;
+	args->outfile = STDOUT_FILENO;
+	args->fdd = fd[0];
+	return (0);
+}
+*/
+
+static int	ft_dup_and_exec(t_args *args, char **command, int last, int fd[2])
+{
+	char	**char_envp;
+	char	*path;
+
+	if (args->infile && dup2(args->infile, STDIN_FILENO) == -1)
+		return (ft_error(13));
+	else if (dup2(args->fdd, STDIN_FILENO) == -1)
+		return (ft_error(13));
+	if (!last && dup2(fd[1], STDOUT_FILENO) == -1)
+		return (ft_error(13));
+	if (args->outfile && dup2(args->outfile, STDOUT_FILENO) == -1)
+		return (ft_error(13));
+	close(fd[0]);
+	if (ft_is_char_builtins(command[0]) && !ft_exec_builtins(args, command))
+		exit(0);
+	path = ft_get_path(args, command[0]);
+	char_envp = ft_get_char_envp(args);
+	if (execve(path, command, char_envp) == -1)
+		return (ft_free_str(char_envp), free(path), ft_error(12));
+	ft_free_str(char_envp);
+	free(path);
+	return (0);
+}
+
+int	ft_execute_child(t_args *args, char **command, int last)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	if (pipe(fd))
+		return (ft_error(11));
+	if (last && args->size == 1 && ft_is_char_builtins(command[0]))
+		return (ft_exec_builtins(args, command));
+	pid = fork();
+	if (pid == -1)
+		return (ft_error(4));
+	if (pid == 0 && ft_dup_and_exec(args, command, last, fd))
+		return (1);
+	close(fd[1]);
+	ft_add_pid(args, pid);
+	args->infile = STDIN_FILENO;
+	args->outfile = STDOUT_FILENO;
+	args->fdd = fd[0];
+	return (0);
+}
+
