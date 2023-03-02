9bca2a4c9f7c70386a25d0b13dfeddc0cab32e5d
Thib1708 <thibault.giraudon@gmail.com>
2023-03-02 15:51:59 +0100

    Add : ft_sign in ft_utils

--- srcs/utils/ft_utils.c
+++ srcs/utils/ft_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thibaultgiraudon <thibaultgiraudon@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:43:07 by elias             #+#    #+#             */
-/*   Updated: 2023/01/26 14:51:47 by elias            ###   ########.fr       */
+/*   Updated: 2023/03/02 15:51:10 by thibaultgir      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,12 @@ void	ft_sig_ignore(int sig)
 	write(1, "\n", 1);
 	rl_replace_line("", 0);
 	rl_redisplay();
+}
+
+void	ft_sign(int sig)
+{
+	(void)sig;
+	rl_on_new_line();
+	write(1, "\n", 1);
+	rl_replace_line("", 0);
 }
