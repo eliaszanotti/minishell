8796379f3df633cd06a6b4851786250bdba08989
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-05-10 18:52:01 +0200

    change files : fix echo -n

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 15:59:43 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/04/27 15:18:48 by elias            ###   ########.fr       */
+/*   Updated: 2023/05/10 18:51:31 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,37 @@ static int	ft_is_flag(char *str)
 	return (0);
 }
 
+static int	ft_print_loop(t_args *args, char **cmd, int n, int first)
+{
+	int	i;
+
+	i = 0;
+	while (cmd[++i])
+	{
+		if (first)
+		{
+			if (ft_is_flag(cmd[i]) && i == 1)
+				n = 1;
+			else
+			{
+				ft_printf_fd("%s", args->outfile, cmd[i]);
+				first = 0;
+			}
+		}
+		else
+			ft_printf_fd(" %s", args->outfile, cmd[i]);
+	}
+	return (n);
+}
+
 int	ft_echo(t_args *args, char **cmd)
 {
 	int	first;
 	int	n;
-	int	i;
 
 	first = 1;
 	n = 0;
-	i = 0;
-	while (cmd[++i])
-	{
-		if (ft_is_flag(cmd[i]))
-			n = 1;
-		else if (!first)
-			ft_printf_fd(" %s", args->outfile, cmd[i]);
-		else
-		{
-			ft_printf_fd("%s", args->outfile, cmd[i]);
-			first = 0;
-		}
-	}
+	n = ft_print_loop(args, cmd, n, first);
 	if (!n)
 		ft_printf_fd("\n", args->outfile);
 	if (!args->last)
