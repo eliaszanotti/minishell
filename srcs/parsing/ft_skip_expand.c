dfbb2053ed4fe555efa3ea9d6ffdb4d076c0b441
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-06-07 12:56:49 +0200

    fix export LS="ls     -l"; $LS

--- srcs/parsing/ft_skip_expand.c
+++ srcs/parsing/ft_skip_expand.c
@@ -56,6 +56,8 @@ static int	ft_add_each_variable(t_list **instruction, char *value)
 		if (!new)
 			return (ft_free_str(values), ft_error(99));
 		ft_lstadd_back(instruction, new);
+		if (values[i + 1] && ft_add_single_str(instruction, NULL))	
+			return (ft_error(99));
 		i++;
 	}
 	return (ft_free_str(values), 0);
