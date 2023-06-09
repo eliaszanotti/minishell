bad48ee8f82f99d5a53e4c9766c1af809cfc55df
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-06-09 10:40:34 +0200

    change heredoc waitpid

--- srcs/exec/ft_heredoc.c
+++ srcs/exec/ft_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_heredoc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 10:18:30 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/06/07 18:27:19 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2023/06/09 10:39:11 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ int	ft_heredoc(t_args *args, char *delimiter)
 		}
 	}
 	close(fd[1]);
-	waitpid(pid, NULL, 1);
+	waitpid(pid, NULL, 0);
 	args->infile = fd[0];
 	return (0);
 }
