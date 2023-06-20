b0180aee1f79283f1a11d1b77e7de3311a0e4711
Thibault Giraudon <tgiraudo@z4r7p7.42lyon.fr>
2023-06-20 18:05:26 +0200

    fix: leaks in heredoc

--- srcs/exec/ft_heredoc.c
+++ srcs/exec/ft_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_heredoc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 13:49:08 by elias             #+#    #+#             */
-/*   Updated: 2023/06/14 13:52:36 by elias            ###   ########.fr       */
+/*   Updated: 2023/06/20 18:03:28 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ static void	ft_exit_heredoc(int sig)
 {
 	(void)sig;
 	write(1, "\n", 2);
+	close(0);
 	g_last_errno = 130;
-	exit(130);
+	// exit(130);
 }
 
-static int	ft_heredoc_loop(char *delimiter, int fd[2])
+static int	ft_heredoc_loop(t_args *args, char *delimiter, int fd[2])
 {
+	t_envp	*envp = args->envp;
 	char	*line;
 
 	signal(SIGINT, ft_exit_heredoc);
@@ -41,6 +43,16 @@ static int	ft_heredoc_loop(char *delimiter, int fd[2])
 		}
 		else
 		{
+			if (g_last_errno == 130)
+			{
+				free(args->pid_tab);
+				free(args->close_tab);
+				while (envp && ft_strcmp(envp->name, "SHLVL"))
+					envp = envp->next;
+				if (envp)
+					free(envp->value);
+				exit(130);
+			}
 			write(1, "minishell: warning: here-document"\
 				" delimited by end-of-file\n", 60);
 			break ;
@@ -63,7 +75,7 @@ int	ft_heredoc(t_args *args, char *delimiter)
 		return (ft_error(1260, NULL));
 	if (pid == 0)
 	{
-		if (ft_heredoc_loop(delimiter, fd))
+		if (ft_heredoc_loop(args, delimiter, fd))
 			return (1);
 		exit(0);
 	}
