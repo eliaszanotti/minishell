508f4bbc728f657751a17c48ad583d77d746099d
Thibault Giraudon <tgiraudo@z4r8p5.42lyon.fr>
2023-06-21 09:53:01 +0200

    fix: 369831? = 2 if builtins last command in pipe

--- srcs/exec/ft_wait_execution.c
+++ srcs/exec/ft_wait_execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_wait_execution.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 14:29:00 by elias             #+#    #+#             */
-/*   Updated: 2023/06/16 14:42:21 by elias            ###   ########.fr       */
+/*   Updated: 2023/06/21 09:28:42 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_wait_execution(t_args *args)
 	{
 		ret = 0;
 		waitpid(args->pid_tab[i], &ret, 0);
-		if (WIFEXITED(ret))
+		if (WIFEXITED(ret) && i == args->size - 1)
 			g_last_errno = WEXITSTATUS(ret);
 		else if (WIFSIGNALED(ret))
 			ft_signals(WTERMSIG(ret));
